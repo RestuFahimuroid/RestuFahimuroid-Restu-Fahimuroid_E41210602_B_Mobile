5859cc13e10a20504a5dd39f9fba284b39b4c1cc
Restu <fahimmuroid@gmail.com>
2023-02-10 15:52:57 +0700

    minggu 1

---
+++ linerLayout/app/src/main/java/com/example/linerlayout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.linerlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner dates = findViewById(R.id.times);
+    }
+}
