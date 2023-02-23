785630b068b0b98d96bf0fcd2df0ac90c071d102
Restu <fahimmuroid@gmail.com>
2023-02-23 23:35:12 +0700

    minggu 3

---
+++ minggu3/app/src/main/java/com/example/minggu3/acara14.java
@@ -0,0 +1,44 @@
+package com.example.minggu3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class acara14 extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    //Set Global Variable
+    ListView listView;
+    ArrayAdapter<CharSequence> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acara14);
+
+        listView = (ListView)findViewById(R.id.listView);
+        adapter = ArrayAdapter.createFromResource(this,R.array.nama_negara,android.R.layout.simple_list_item_1);
+        //adapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.list, menu);
+        return true;
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this, adapter.getItem(position), Toast.LENGTH_SHORT).show();
+
+    }
+}
